b13ad4b36cf727400888623939845eb5dd6dd251
vamshi-1216 <bvamshi1817@gmail.com>
2025-12-07 18:33:18 +0530

    Docker removed in Discovery-service

--- frontend/admin-app/cricket-frontend/src/api/axiosConfig.js
+++ frontend/admin-app/cricket-frontend/src/api/axiosConfig.js
@@ -1,10 +1,10 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: import.meta.env.VITE_GATEWAY_URL,
+  baseURL: "https://team-production-35c9.up.railway.app",
   headers: {
     "Content-Type": "application/json",
-  }
+  },
 });
 
 export default axiosInstance;
